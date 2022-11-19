1d8603a671dbb280a531d99620c69fe4c91475da
Wadevelop <deersat@hotmail.com>
2022-11-19 00:49:29 +0000

    This my first program with Github

---
+++ src/JavaMyWork.java
@@ -0,0 +1,7 @@
+public class JavaMyWork {
+    public static void main(String[]args){
+        //Linking Intelij with Github
+        System.out.println("This is my first Program");
+        System.out.println("18Nov2022");
+    }
+}
