caef46a4266a160b0f64d389960c1e722f2f078a
Wadevelop <deersat@hotmail.com>
2022-11-20 13:43:39 +0000

    This my first program with Github

--- src/JavaMyWork.java
+++ src/JavaMyWork.java
@@ -1,10 +1,10 @@
 public class JavaMyWork {
     public static void main(String[]args){
-        //calculate rectangle area
-        System.out.println("This is rectangle area");
-        int width=5;
-        int height=6;
-        int area=width*height;
-        System.out.print("area of rectangle="+area);
+        System.out.println(".............");
+        System.out.println("|           |");
+        System.out.println("|           |");
+        System.out.println("|           |");
+        System.out.println(".............");
+
     }
 }
